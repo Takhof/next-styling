9491c9c61de9f0a4e55a41b5f92768149e6dfe8b
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-10-29 20:44:50 +0900

    feat: add styling

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import styles from "../styles/About.module.css";
+
+function About() {
+  return (
+    <div className={styles.highlight}>
+      About Page
+      <button className="btn btn-success">Primary button </button>
+    </div>
+  );
+}
+
+export default About;
