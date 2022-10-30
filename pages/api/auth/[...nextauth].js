b71db065d3e76272db39e4eed7812693f61c2b9a
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-10-30 21:44:12 +0900

    feat: add 3 auth methods

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CognitoProvider from "next-auth/providers/cognito";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      issuer: process.env.COGNITO_ISSUER,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
