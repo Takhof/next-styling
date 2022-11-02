f774d52150314f9f8a59478ae9c611c2dbbc3ecf
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-11-02 18:19:39 +0900

    feat: add timestamp to test isr on aws

--- pages/index.js
+++ pages/index.js
@@ -2,53 +2,24 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSession } from "next-auth/react";
 
-export default function Home() {
+export default function Home(props) {
   const { data: session, data, status } = useSession();
   console.log(data, session, status);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h2>
           {session ? `${session.user.name}, ` : ""}
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          Welcome to <a href="http://takhof.github.io">Tom</a>'s Test Page!
+        </h2>
+        <p></p>
+        <h3>
+          <p>Testing ISR on AWS....</p>
+          <p>
+            Current time is {props.timestamp} milliseconds since the beginning
+            of time.
+          </p>
+        </h3>
       </main>
 
       <footer className={styles.footer}>
@@ -66,3 +37,12 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps = async (context) => {
+  return {
+    props: {
+      timestamp: new Date().getTime(),
+    },
+    revalidate: 10,
+  };
+};
